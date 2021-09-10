4eac31b5dd2833919e111aebe198afa993b2fd01
Miguel Bermudo <mbermudo@us.es>
2021-09-10 14:08:59 +0200

    started doing some more login stuff

--- TableToppings server/concurrTCPServer.go
+++ TableToppings server/concurrTCPServer.go
@@ -11,12 +11,19 @@ import (
 	// "encoding/base64"
 )
 
-var connectedClients = 0 // number of currently connected clients to the server.
+type user_conn struct {
+	connection net.Conn
+	user       User
+}
+
+var connections []user_conn // current connections
 
 func handleConnection(c net.Conn) {
-	fmt.Print("new connection detected, current number of connected clients is")
-	fmt.Print(connectedClients)
-	fmt.Print("\n")
+	fmt.Print("new connection accepted, current connected clients are:\n")
+	for _, c := range connections {
+		fmt.Print(c.connection.RemoteAddr())
+		fmt.Print("\n")
+	}
 
 	for {
 		netData, err := bufio.NewReader(c).ReadString('\n')
@@ -31,13 +38,24 @@ func handleConnection(c net.Conn) {
 			break
 		}
 		fmt.Println(temp)
-		counter := strconv.Itoa(connectedClients) + "\n"
+		counter := strconv.Itoa(len(connections)) + "\n"
 		c.Write([]byte(string(counter)))
 	}
 	c.Close()
 }
 
+func addressIsConnected(address string) bool {
+	for _, e := range connections {
+		if address == strings.Split(e.connection.RemoteAddr().String(), ":")[0] {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+
+	//Load configuration.
 	config := LoadConfig()
 	fmt.Println(config)
 	// fmt.Println(config.Users)
@@ -55,13 +73,26 @@ func main() {
 	// defer makes it so the server closes if the main function is exited.
 	defer l.Close()
 
-	for {
+	for { // for with nothing else attached acts like a while(true)
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
+		ip := strings.Split(c.RemoteAddr().String(), ":")[0]
+		// check the remote adress is not any of the currently
+		// connected users.
+		if addressIsConnected(ip) {
+			return
+		}
+
+		var conn user_conn
+		conn.connection = c
+
+		//check credentials for initial connection
+
+		connections = append(connections, conn)
 		go handleConnection(c)
-		connectedClients++
 	}
 }
