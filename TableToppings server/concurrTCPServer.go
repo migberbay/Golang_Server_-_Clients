f210ec405ac7e61b4f9e14abbc992114e1da3741
miguel bermudo bayo <migberbay@gmail.com>
2021-10-18 01:34:17 +0200

    added stuff

--- TableToppings server/concurrTCPServer.go
+++ TableToppings server/concurrTCPServer.go
@@ -6,6 +6,9 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"time"
+
+	"gitlab.com/NebulousLabs/go-upnp"
 	// "encoding/json"
 	// "crypto/sha1"
 	// "encoding/base64"
@@ -17,10 +20,18 @@ type user_conn struct {
 }
 
 var connections []user_conn // current connections
+var external_ip string
+
+func logger_message(conn net.Conn, message string) {
+	t := time.Now()
+	formatted := fmt.Sprintf("%02d-%02d-%d %02d:%02d:%02d",
+		t.Day(), t.Month(), t.Year(),
+		t.Hour(), t.Minute(), t.Second())
+	fmt.Println("[", conn.RemoteAddr(), formatted, "]: ", message)
+}
 
 func handleConnection(conn user_conn, config Config) {
 	current_connection := conn.connection
-	fmt.Print("new connection accepted\n")
 
 	// AUTHENTICATION PROCESS
 	authed, err := AuthenticationHandler(conn, config)
@@ -58,9 +69,7 @@ func AuthenticationHandler(conn user_conn, config Config) (bool, error) {
 
 	authed := false
 	for !authed {
-		fmt.Println("awaiting login attempt for connection:")
-		fmt.Println(current_connection.RemoteAddr())
-
+		logger_message(current_connection, "awaiting login attempt")
 		cont := true
 
 		netData, err := bufio.NewReader(current_connection).ReadString('\n')
@@ -71,8 +80,11 @@ func AuthenticationHandler(conn user_conn, config Config) (bool, error) {
 		}
 
 		temp := strings.TrimSpace(string(netData))
-		fmt.Println(temp)
+
 		log_in_attempt := strings.Split(temp, ":")
+		msg := "Login attempt => "
+		msg = msg + temp
+		logger_message(current_connection, msg)
 
 		if log_in_attempt[0] != "Login" {
 			current_connection.Write([]byte(string("400: Invalid login attempt.\n")))
@@ -87,16 +99,10 @@ func AuthenticationHandler(conn user_conn, config Config) (bool, error) {
 			for _, e := range config.Users {
 				if e.Username == usr {
 					if e.Password == pass { // peferably add some type of cyper to this.
-
-						if addressIsConnected(current_connection.RemoteAddr().String()) {
-							current_connection.Write([]byte(string("400: User is already connected, something went wrong.")))
-							break
-						}
-
 						current_connection.Write([]byte(string("001:accepted;" + strconv.Itoa(e.ID) + "," + e.Type + "," + e.Username)))
 						authed = true
 
-						fmt.Print("Authentication accepted, current connections are:")
+						logger_message(current_connection, "Authentication accepted, current connections are:")
 						for _, c := range connections {
 							if c.connection.RemoteAddr() == conn.connection.RemoteAddr() {
 								c.user.ID = e.ID
@@ -104,8 +110,7 @@ func AuthenticationHandler(conn user_conn, config Config) (bool, error) {
 								c.user.Type = e.Type
 							}
 
-							fmt.Printf("%s - %s (%s)", c.connection.RemoteAddr(), c.user.Username, c.user.Type)
-							fmt.Print("\n")
+							fmt.Printf("%s - %s (%s)\n", c.connection.RemoteAddr(), c.user.Username, c.user.Type)
 						}
 					}
 					break
@@ -123,22 +128,47 @@ func AuthenticationHandler(conn user_conn, config Config) (bool, error) {
 
 func addressIsConnected(address string) bool {
 	for _, e := range connections {
-		if address == strings.Split(e.connection.RemoteAddr().String(), ":")[0] {
+		if address == e.connection.RemoteAddr().String() {
 			return true
 		}
 	}
 	return false
 }
 
+func forwardConfigPort(config Config) {
+	d, err := upnp.Discover()
+	if err != nil {
+		fmt.Println("Discovering router failed, network failure?")
+	}
+
+	external_ip, err = d.ExternalIP()
+	if err != nil {
+		fmt.Println("Extracting external ID failed, is UPnP allowed by your router?")
+	} else {
+		fmt.Println("External IP is ", external_ip)
+	}
+
+	port_i64, err := strconv.ParseUint(config.Port, 10, 16)
+	port_i16 := uint16(port_i64)
+
+	//Example of port forwarding, this probably neesd to be moved to someplace else.
+	err = d.Forward(port_i16, "TableToppings Server")
+	if err != nil {
+		fmt.Println("Error Forwarding port")
+	} else {
+		fmt.Println("Forwarded port ", port_i16, " ready for external connections.")
+	}
+}
+
 func main() {
 	//Load configuration.
 	config := LoadConfig()
-	// fmt.Println(config)
-	fmt.Println(config.Users)
-	fmt.Println(config.Worlds)
+	fmt.Println("Users: ", config.Users)
+	fmt.Println("Worlds: ", config.Worlds)
 
 	PORT := ":" + config.Port
-	l, err := net.Listen("tcp4", PORT)
+
+	listener, err := net.Listen("tcp4", PORT)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -147,27 +177,28 @@ func main() {
 	}
 
 	// defer makes it so the server closes if the main function is exited.
-	defer l.Close()
+	defer listener.Close()
 
 	for { // for with nothing else attached acts like a while(true)
-		fmt.Println("ready for next connection")
-		c, err := l.Accept()
+		c, err := listener.Accept()
+
 		if err != nil {
 			fmt.Println(err)
 			return
+		} else {
+			fmt.Print("Accepted connection ", c.RemoteAddr(), "\n")
 		}
 
-		ip := strings.Split(c.RemoteAddr().String(), ":")[0]
-		// check the remote adress is not any of the currently
-		// connected users.
-		if addressIsConnected(ip) {
+		// check the remote adress is not any of the currently connected sockets. (ip:port)
+		if addressIsConnected(c.RemoteAddr().String()) {
 			return
 		}
 
 		var conn user_conn
 		conn.connection = c
-
 		connections = append(connections, conn)
+		fmt.Println("Ready for next connection")
+
 		go handleConnection(conn, config)
 	}
 }
